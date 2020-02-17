9845859237f42661e22ab74958970423462d55d4
Ivy Nzioka <ivy.nzioka@Ivys-MacBook-Pro.local>
2020-02-17 17:18:56 +0200

    ft(all-games): Add all games component
    - Scafold app folder structure
    - Setup redux for the app
    - add all games component
    - Add all games styling

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+import HeroImage from '../elements/HeroImage/HeroImage';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+               
+            </div>
+    
+        )
+    }
+    }
+
+export default Home
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+module.exports = env => {
+  return {
+    mode:"development",
+    entry: path.resolve(__dirname, 'src/index'),
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    devServer: {
+      contentBase:  path.resolve(__dirname, 'src/'),
+      historyApiFallback: true,
+      port: 9000
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      })
+    ],
+    node: {
+      fs: 'empty'
+    }
+  }
+};
