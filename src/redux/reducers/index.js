02bb96ef490789bc96c999e6b1e7f9857298259b
Ivy Nzioka <ivy.nzioka@Ivys-MacBook-Pro.local>
2020-02-17 18:49:33 +0200

    ft(all creators component): Add all creators component
    - Add all creators component
    - Add all creators component styling
    - Setup redux structure for all creator component
    
    [Finishes]

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import allGamesReducer from './allGamesReducer.js';
 import gameDetailsReducer from './gameDetailsReducer.js';
+import allCreatorsReducer from './allCreatorsReducer.js';
 
 export default combineReducers({
     allGames: allGamesReducer,
     gameDetails: gameDetailsReducer,
+    allCreators: allCreatorsReducer,
 
 });
